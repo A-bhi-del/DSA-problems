ac11ea5950e2735496675f321dc973c24c25c94b
Abhishek Gupta <guptaabhishek60420@gmail.com>
2026-07-22 00:37:29 +0530

    Add Minimum Deletions to Make Sorted

---
+++ Minimum Deletions to Make Sorted/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    int minDeletions(vector<int>& arr) {
+        // code here
+        vector<int>temp;
+        int n = arr.size();
+        
+        for(int i = 0; i < n; i++){
+            int s = 0;
+            int e = temp.size();
+            
+            while(s < e){
+                int mid = s + (e - s)/2;
+                
+                if(temp[mid] < arr[i]){
+                    s = mid + 1;
+                }else{
+                    e = mid;
+                }
+            }
+            
+            if(s == temp.size()){
+                temp.push_back(arr[i]);
+            }else if(temp[s] > arr[i]){
+                temp[s] = arr[i];
+            }
+        }
+        
+        return n - temp.size();
+    }
+};
