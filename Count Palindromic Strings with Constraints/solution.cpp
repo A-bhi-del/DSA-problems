a907da43275d9bff13db31ee743c76760a7018ea
Abhishek Gupta <guptaabhishek60420@gmail.com>
2026-09-01 19:10:04 +0530

    Add Count Palindromic Strings with Constraints

---
+++ Count Palindromic Strings with Constraints/solution.cpp
@@ -0,0 +1,53 @@
+class Solution {
+  public:
+    int mod = 1e9+7;
+    vector<int>fact;
+    int compute(int colors, int parts){
+        long long ans = 1;
+
+        for(int i = 0; i < parts; i++){
+            ans = ans * (colors - i) % mod;
+        }
+
+        return ans;
+    }
+    
+    int palindromicStrings(int n, int k) {
+        // code here
+        fact.resize(28, 1);
+        for(int i = 2; i <= 26; i++){
+            fact[i] = (fact[i-1] % mod * i % mod) % mod;
+        }
+        
+        vector<vector<int>>dp(k+1, vector<int>(n+1, 0));
+        
+        for(int i = 1; i <= k; i++){
+            dp[i][1] = i; 
+        }
+        
+        for(int i = 1; i <= k; i++){
+            for(int j = 2; j <= n; j++){
+                if(i >= (j+1)/2){
+                    dp[i][j] = (dp[i][j-1] + compute(i, (j+1)/2)) % mod;
+                }else{
+                    dp[i][j] = dp[i][j-1];
+                }
+            }
+        }
+        
+        // for(int i = 1; i <= k; i++){
+        //     for(int j = 1; j <= n; j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+        return dp[k][n];
+    }
+};
+
+/*
+1 2 3 4 5 6 7 8 9 
+2 4 6 8 8 8 8 8 8
+3 6 12 18 
+*/
