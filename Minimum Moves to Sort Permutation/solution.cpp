820d9339e04abd4709c379e75e25869967972134
Abhishek Gupta <guptaabhishek60420@gmail.com>
2026-09-03 03:15:34 +0530

    Add Minimum Moves to Sort Permutation

--- Minimum Moves to Sort Permutation/solution.cpp
+++ Minimum Moves to Sort Permutation/solution.cpp
@@ -4,20 +4,20 @@ class Solution {
         // code here
         int n = arr.size();
         
-        vector<int>count(n+1, 0);
+        vector<int>dp(n+1, 0);
         
         for(int i = 0; i < n; i++){
-            if(count[arr[i] - 1] != 0){
-                count[arr[i]] += 1 + count[arr[i] - 1];
+            if(dp[arr[i] - 1] != 0){
+                dp[arr[i]] += 1 + dp[arr[i] - 1];
             }else{
-                count[arr[i]] = 1;
+                dp[arr[i]] = 1;
             }
         }
         
         int LCS = 0;
         
         for(int i = 0; i <= n; i++){
-            LCS = max(LCS, count[i]);
+            LCS = max(LCS, dp[i]);
         }
         
         return n - LCS;
